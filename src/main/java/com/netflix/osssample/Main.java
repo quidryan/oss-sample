a334496c1dfb103d2a849e9afba851250a61488e
Justin Ryan <jryan@netflix.com>
2013-03-22 00:28:05 -0700

    Adding some basic source

---
+++ src/main/java/com/netflix/osssample/Main.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+
+public class Main {
+    /**
+     * Count characters in stdin or a provided file.
+     */
+    public static void main(String args[]) throws java.io.IOException, java.io.FileNotFoundException {
+        int count = 0;
+        InputStream is;
+
+        if (args.length == 1)
+            is = new FileInputStream(args[0]);
+        else
+            is = System.in;
+    
+        while (is.read() != -1)
+            count++;
+
+        if (args.length == 1)
+            System.out.println(args[0] + " has "+ count + " chars.");
+        else
+            System.out.println("Input has " + count + " chars.");
+    }
+}
